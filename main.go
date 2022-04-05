884fb93e3bd0feb10ea4d73f047022bec390f019
ruthlessgang <49008555+ruthlessgang@users.noreply.github.com>
2022-04-05 13:28:02 +0530

    Update main.go

--- main.go
+++ main.go
@@ -80,6 +80,13 @@ type frontendServer struct {
 	adSvcConn *grpc.ClientConn
 }
 
+func mustMapEnv(target *string, envKey string) {
+	v := os.Getenv(envKey)
+	if v == "" {
+		panic(fmt.Sprintf("environment variable %q not set", envKey))
+	}
+	*target = v
+}
 func main() {
 	ctx := context.Background()
 	log := logrus.New()
@@ -253,13 +260,6 @@ func initProfiling(log logrus.FieldLogger, service, version string) {
 	log.Warn("warning: could not initialize Stackdriver profiler after retrying, giving up")
 }
 
-func mustMapEnv(target *string, envKey string) {
-	v := os.Getenv(envKey)
-	if v == "" {
-		panic(fmt.Sprintf("environment variable %q not set", envKey))
-	}
-	*target = v
-}
 
 func mustConnGRPC(ctx context.Context, conn **grpc.ClientConn, addr string) {
 	var err error
